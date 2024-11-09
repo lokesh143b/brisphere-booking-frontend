4d422d0d9b2ab78ceff885172a451111485ddfe3
lokesh143b <138014740+lokesh143b@users.noreply.github.com>
2024-11-09 20:11:18 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import { FormProvider } from "./context/formContext.jsx";
+import { FormProvider } from "./context/FormContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
